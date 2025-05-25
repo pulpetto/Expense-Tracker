7f00bce45e5b627bb9c4f37241f903f88ac79363
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-25 06:12:28 +0200

    add widget

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+        ],
+      ),
+    );
+  }
+}
