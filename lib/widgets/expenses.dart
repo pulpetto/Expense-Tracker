bf26507f50ec845fec8afd339679a1cd4d27b93e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-26 18:47:11 +0200

    show snackbar when expense deleted

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,9 +41,28 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
