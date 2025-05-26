be21299823fdff4b7127c07a2d2df8757bdca5a3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-26 20:21:13 +0200

    add theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Expenses());
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    );
   }
 }
